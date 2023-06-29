151608c784d5ce96916f8f98cf5301aeb9bf9060
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-06-29 22:09:08 +0700

    Register Area, Nav Scroll & Details

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 // Add custom JavaScript here
+function useScroll() {
+  const navbar = document.querySelector('.navbar');
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add('bg-dark')
+    } else {
+      navbar.classList.remove('bg-dark')
+    }
+  })
+}
+
+
+document.addEventListener('DOMContentLoaded', useScroll)
