c9eab4525cad21e0768a5a1f622f75f4ac7c199f
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-06-29 23:40:39 +0700

    Invitation & Takeaways

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 // Add custom JavaScript here
+
+const navbarCollapse = document.querySelector('.navbar-collapse')
+const navbar = document.querySelector('.navbar');
 function useScroll() {
-  const navbar = document.querySelector('.navbar');
+
   window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
       navbar.classList.add('bg-dark')
@@ -8,7 +11,9 @@ function useScroll() {
       navbar.classList.remove('bg-dark')
     }
   })
-}
 
+
+}
+navbar.addEventListener('onmouseleave', () => navbarCollapse.style.visibility = 'hidden')
 
 document.addEventListener('DOMContentLoaded', useScroll)
